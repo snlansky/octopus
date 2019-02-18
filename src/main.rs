c6f0afa836fee4022b78e73acf3886a0d12a890f
snlansky <snlan@live.cn>
2019-02-18 22:04:13 +0800

    add command line

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,40 @@ extern crate zookeeper;
 extern crate log;
 extern crate env_logger;
 extern crate threadpool;
-
+extern crate clap;
 
 
 use config::config::init;
+use clap::{Arg, App, SubCommand};
+
 
 mod dal;
 mod config;
 mod discovery;
 
-
 fn main() {
     env_logger::init();
     init("service.json".to_string());
     debug!("ok");
+
+    let matches = App::new("octopus")
+        .version("1.0")
+        .author("snlan@live.cn")
+        .about("data bus")
+        .arg(Arg::with_name("cluster")
+            .short("c")
+            .long("cluster")
+            .value_name("ADDRESS")
+            .required(true)
+            .help("the zookeeper cluster address."))
+        .arg(Arg::with_name("path")
+            .short("p")
+            .long("path")
+            .required(true)
+            .help("the config path at zookeeper."))
+        .get_matches();
+
+    let cluster = matches.value_of("path").unwrap();
+    println!("{:?}", );
 }
 

61def8fc76ba43167395f468ee12af3852be29f5
snlansky <snlan@live.cn>
2019-03-02 20:54:37 +0800

    add condition compile protobuf

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
 extern crate protoc_rust_grpc;
 
 fn main() {
+    use std::env;
+    if env::consts::OS != "windows" {
+        build_protos();
+    }
+}
+
+fn build_protos() {
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
         out_dir: "src/proto",
         includes: &["proto"],
